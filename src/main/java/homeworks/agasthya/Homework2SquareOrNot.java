7cc5335ebf5ed8cd6893e0d10e9f608005bab471
Yesnt420 <72351137+Yesnt420@users.noreply.github.com>
2020-11-15 11:11:12 -0500

    Check if its square or not using length and breadth

---
+++ src/main/java/homeworks/agasthya/Homework2SquareOrNot.java
@@ -0,0 +1,41 @@
+/*      x= ("Enter your first number :"))
+
+        x= ("Enter your second number :"))
+
+        if x==y:
+
+        print("Its a square")
+
+        else :
+
+        print("its an rectangle")*/
+
+package homeworks.agasthya;
+import java.util.Scanner;
+public class Homework2SquareOrNot{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter length:");
+        int x = s.nextInt();
+        System.out.println("Enter breadth:");
+        int y = s.nextInt();
+        if(x==y){
+            System.out.println("Square");
+        }
+        else{
+            System.out.println("Rectangle");
+        }
+    }
+}
+
+/*x=int(input("Enter your first number :"))
+
+        x=int(input("Enter your second number :"))
+
+        if x=y:
+
+        print("Its a square")
+
+        else :
+
+        print("its an rectangle")*/
