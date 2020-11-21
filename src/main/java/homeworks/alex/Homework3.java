56684b642079f62b82de8f83a8da9e92cbe61f8e
Alex <abmorris3316@gmail.com>
2020-11-20 20:36:49 -0500

    Testing

--- src/main/java/homeworks/alex/Homework3.java
+++ src/main/java/homeworks/alex/Homework3.java
@@ -9,6 +9,12 @@ public class Homework3 {
 
             System.out.println("Enter a 4-Digit Number:");
             int x = input.nextInt();
+
+            int digit1 = x % 10;
+            int digit2 = (x / 10) % 10;
+            int digit3 = (x / 100) % 10;
+            int digit4 = (x / 1000) % 10;
+            System.out.println(digit1 + "" + digit2 + "" + digit3 + "" + digit4);
         }
     }
 
