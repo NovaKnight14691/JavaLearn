d17ecf30528ce5cc7595d9f43d5a4bb6f737352b
Alex <abmorris3316@gmail.com>
2020-11-20 21:22:24 -0500

    11/13/20 Homework Assignment 2

--- src/main/java/homeworks/alex/ConditionalHomework2.java
+++ src/main/java/homeworks/alex/ConditionalHomework2.java
@@ -2,7 +2,7 @@ package homeworks.alex;
 
 import java.util.Scanner;
 
-public class Homework2 {
+public class ConditionalHomework2 {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
