422717b0fe98e6ea0d582f21cb6010a25cd3e6ad
Alex <abmorris3316@gmail.com>
2020-11-20 20:09:38 -0500

    Testing

---
+++ src/main/java/homeworks/alex/Homework1.java
@@ -0,0 +1,24 @@
+package homeworks.alex;
+
+import java.util.Scanner;
+
+public class Homework1 {
+
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+
+            System.out.println("Enter Length of Polygon:");
+            int x = input.nextInt();
+
+            System.out.println("Enter Breadth of Polygon:");
+            int y = input.nextInt();
+
+            if (x == y) {
+                System.out.println("This Shape is a Square");
+
+            } else {
+                System.out.println("This Shape is a Rectangle");
+
+            }
+        }
+    }
---
+++ src/main/java/homeworks/alex/Homework3.java
@@ -0,0 +1,14 @@
+package homeworks.alex;
+
+import java.util.Scanner;
+
+public class Homework3 {
+
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+
+            System.out.println("Enter a 4-Digit Number:");
+            int x = input.nextInt();
+        }
+    }
+
