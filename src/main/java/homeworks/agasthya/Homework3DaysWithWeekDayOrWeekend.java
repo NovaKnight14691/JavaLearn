549c6a4ae2a57c5218657203b7ed07b920ad57e5
Yesnt420 <72351137+Yesnt420@users.noreply.github.com>
2020-12-04 18:08:17 -0500

    Homeworks

--- src/main/java/homeworks/agasthya/Homework3DaysWithWeekDayOrWeekend.java
+++ src/main/java/homeworks/agasthya/Homework3DaysWithWeekDayOrWeekend.java
@@ -6,43 +6,48 @@ public class Homework3DaysWithWeekDayOrWeekend {
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-        System.out.print("Input the number you want for either weekday or weekend: ");
+        System.out.print("Input the number you want for the day: ");
         int day = in.nextInt();
-
 
         System.out.println(TheNameOfTheDay(day));
     }
+
 
 
     public static String TheNameOfTheDay(int day) {
         String NameOfTheDay = " ";
         switch (day) {
             case 1:
-                NameOfTheDay = "Weekend";
+                NameOfTheDay = "Sunday";
+                System.out.println("Weekend");
                 break;
             case 2:
-                NameOfTheDay = "Weekday";
+                NameOfTheDay = "Monday";
+                System.out.println("Weekday");
                 break;
             case 3:
-                NameOfTheDay = "Weekday";
+                NameOfTheDay = "Tuesday";
+                System.out.println("Weekday");
                 break;
             case 4:
-                NameOfTheDay = "Weekday";
+                NameOfTheDay = "Wednesday";
+                System.out.println("Weekday");
                 break;
             case 5:
-                NameOfTheDay = "Weekday";
+                NameOfTheDay = "Thursday";
+                System.out.println("Weekday");
                 break;
             case 6:
-                NameOfTheDay = "Weekday";
+                NameOfTheDay = "Friday";
+                System.out.println("Weekday");
                 break;
             case 7:
-                NameOfTheDay = "Weekend";
+                NameOfTheDay = "Saturday";
+                System.out.println("Weekend");
                 break;
-            default:NameOfTheDay = "Invalid input";
+            default:NameOfTheDay = "Invalid Day";
                 break;
         }
-
-
 
 
         return NameOfTheDay;
