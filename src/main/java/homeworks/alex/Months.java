519954e067b712073fdfcc11e5e5f59846da2213
Alex <abmorris3316@gmail.com>
2020-12-04 20:30:13 -0500

    11/20/20 Homework Assignment 4

---
+++ src/main/java/homeworks/alex/Months.java
@@ -0,0 +1,54 @@
+package homeworks.alex;
+
+import java.util.Scanner;
+
+public class Months {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        String num = input.nextLine();
+
+        switch (num) {
+            case "1":
+                System.out.println("January");
+                break;
+            case "2":
+                System.out.println("February");
+                break;
+            case "3":
+                System.out.println("March");
+                break;
+            case "4":
+                System.out.println("April");
+                break;
+            case "5":
+                System.out.println("May");
+                break;
+            case "6":
+                System.out.println("June");
+                break;
+            case "7":
+                System.out.println("July");
+                break;
+            case "8":
+                System.out.println("August");
+                break;
+            case "9":
+                System.out.println("September");
+                break;
+            case "10":
+                System.out.println("October");
+                break;
+            case "11":
+                System.out.println("November");
+                break;
+            case "12":
+                System.out.println("December");
+                break;
+            default:
+                System.out.println("Number is out of requested range.");
+        }
+    }
+}
